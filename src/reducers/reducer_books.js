b2936da4411bee6ff2a4e5a0b21112aacb91d698
Andy <russellandy98@gmail.com>
2019-09-11 17:30:02 -0400

    Tutorial 2 finished, redux installed and reducers made

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: "JavaScript: The Good Parts", pages: 101},
+        {title: "The Dark Tower", pages: 20},
+        {title: "Eloquent NodeJS", pages: 45},
+        {title: "Book Clickers for Dummies", pages: 231}
+    ];
+}
